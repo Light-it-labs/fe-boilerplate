b89345a871169e0bfd92621aeff42e5a759026ea
Carolyn Symonds <carolyn.symonds@lightit.io>
2023-11-28 13:29:30 -0300

    refactor: removed protected route and user state validation on routing

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,11 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import type { Location } from "react-router-dom";
 
 import { Layout } from "~/layout";
 import { Home, Login, NotFound } from "~/screens";
 import { ModalRouter } from "./ModalRouter";
-import { ProtectedRoute } from "./ProtectedRoute";
 import { ROUTES } from "./routes";
+import { useUserStore } from "~/stores";
 
 export const Router = () => {
   const location = useLocation();
@@ -13,21 +13,36 @@ export const Router = () => {
     previousLocation?: Location;
   };
 
+  const userState = useUserStore((state) =>
+    state.token ? "loggedIn" : "loggedOut",
+  );
+
   return (
     <>
       <Routes location={previousLocation ?? location}>
         {/* PUBLIC ONLY ROUTES */}
-        <Route element={<ProtectedRoute expected="loggedOut" />}>
-          <Route element={<Login />} path={ROUTES.login} />
-        </Route>
+        {userState === "loggedOut" && (
+          <>
+           <Route
+              element={<Navigate to={ROUTES.login} replace />}
+              path={"*"}
+            />
+            <Route element={<Login />} path={ROUTES.login} />
+          </>
+        )}
         {/* PRIVATE ONLY ROUTES */}
-        <Route element={<ProtectedRoute expected="loggedIn" />}>
-          <Route path="*" element={<NotFound />} />
-
-          <Route element={<Layout />}>
-            <Route element={<Home />} path={ROUTES.home} />
-          </Route>
-        </Route>
+        {userState === "loggedIn" && (
+         <>
+            <Route
+                element={<Navigate to={ROUTES.home} />}
+                path={"*"}
+              />
+            <Route path="*" element={<NotFound />} />
+            <Route element={<Layout />}>
+              <Route element={<Home />} path={ROUTES.home} />
+            </Route>
+          </>     
+            )}
       </Routes>
       <Routes>
         <Route
