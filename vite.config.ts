7b5b97b4eb3b7c39132c5ce5d085f50611d7928b
juantejer4 <68563891+juantejer4@users.noreply.github.com>
2024-01-03 14:13:18 -0300

    ref: improve readability adding line breaks
    Co-authored-by: Gianfranco Rocco <110541583+gianfranco-rocco@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,13 @@ const config = ({ mode }: ConfigEnv): UserConfigExport => {
   // Load and merge environment variables
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   
-  const { VITE_SENTRY_AUTH_TOKEN, VITE_SENTRY_ORGANIZATION, VITE_SENTRY_PROJECT } = process.env;
+  const { 
+    VITE_SENTRY_AUTH_TOKEN, 
+    VITE_SENTRY_ORGANIZATION, 
+    VITE_SENTRY_PROJECT,
+    VITE_APP_ENV,
+    VITE_APP_URL
+   } = process.env;
 
   const baseConfig = {
     plugins: [
