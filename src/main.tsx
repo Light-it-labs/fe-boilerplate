41410c3f4d738ea6a083345085ffb0a56dbc41d9
Juan Tejera <juan.tejera@lightit.io>
2024-01-05 15:20:21 -0300

    feat: simplify fallback component

--- src/main.tsx
+++ src/main.tsx
@@ -64,9 +64,7 @@ createRoot(root).render(
     <QueryClientProvider client={queryClient}>
       <GoogleOAuthProvider clientId={env.VITE_GOOGLE_AUTH_SSO_CLIENT_ID}>
         <Sentry.ErrorBoundary fallback={
-          <ErrorBoundaryFallback title="An error has occurred">
-            This is a basic usage, you can add an error code too!
-          </ErrorBoundaryFallback>
+          <ErrorBoundaryFallback/>
           }>
           <BrowserRouter>
             <Router />
--- src/screens/ErrorBoundaryFallback.tsx
+++ src/screens/ErrorBoundaryFallback.tsx
@@ -2,20 +2,13 @@ import React from "react";
 
 import { LightitLogo } from "~/assets";
 
-interface ErrorBoundaryFallbackProps {
-  errorCode?: string | number;
-  title: string;
-  children?: React.ReactNode;
-}
-export const ErrorBoundaryFallback: React.FC<ErrorBoundaryFallbackProps> = ({ errorCode, title, children }) => {
+export const ErrorBoundaryFallback =  () => {
   return (
     <main className="flex h-screen flex-col items-center justify-center">
       <div className="mb-20">
         <img src={LightitLogo} alt="logo" />
       </div>
-      <h1 className="mb-5 text-5xl font-bold">{errorCode}</h1>
-      <h2 className="mb-10 text-2xl">{title}</h2>
-      {children && <p className="mb-10 text-xl text-gray-500">{children}</p>}
+      <h2 className="mb-10 text-2xl">Something went terribly wrong!</h2>
     </main>
   );
 };
