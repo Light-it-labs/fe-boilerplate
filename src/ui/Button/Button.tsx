5a6402edfd62329bfd503480b5495f59f8be162d
Andrés Nakanishi <andres.nakanishi@lightit.io>
2024-03-08 13:51:20 -0300

    [DELETE] Removed that line

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -9,7 +9,6 @@ export const buttonVariants = [
   "outline",
   "outline-white",
   "secondary",
-  "secondary-link",
   "tertiary-link",
 ] as const;
 export type ButtonVariant = (typeof buttonVariants)[number];
