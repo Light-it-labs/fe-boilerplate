8f9dce9122d907bebf80276ed0dcf354610ffcfa
Naka <andres.nakanishi@lightit.io>
2024-09-26 10:23:10 -0300

    HIPAA Compliance fix

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -1,14 +1,7 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-export type User = {
-  email: string;
-  name: string;
-};
-
 export type AuthStoreState = {
-  user: User | null;
-  setUser(user: User | null): void;
   token: string | null;
   setToken(token: string | null): void;
 };
@@ -16,17 +9,13 @@ export type AuthStoreState = {
 export const useAuthStore = create<AuthStoreState>()(
   persist(
     (set) => ({
-      user: null,
       token: null,
-      setUser: (user: User | null) => {
-        set(() => ({ user }));
-      },
       setToken: (token: string | null) => {
         set(() => ({ token }));
       },
     }),
     {
-      name: "feedbackAuthStore",
+      name: "authStore",
     },
   ),
 );
