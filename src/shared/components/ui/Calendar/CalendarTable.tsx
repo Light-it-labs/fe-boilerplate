da749964c9b7cdb22816745ee4e2542b74f535c9
walt-it <walter@lightit.io>
2024-10-16 00:12:39 -0300

    feat: use tv for cell styles

--- src/shared/components/ui/Calendar/CalendarTable.tsx
+++ src/shared/components/ui/Calendar/CalendarTable.tsx
@@ -8,12 +8,28 @@ import {
 } from "react-aria-components";
 import { tv } from "tailwind-variants";
 
-import { tw } from "~/shared/utils";
-
 const calendarTable = tv({
   slots: {
     root: "w-full md:w-fit",
     dayOfWeek: "pb-1 text-xs font-semibold capitalize text-slate-400",
+  },
+});
+
+const calendarCell = tv({
+  base: "mx-auto my-0.5 flex aspect-square w-8 items-center justify-center rounded-full text-sm font-semibold text-gray-600 sm:w-11",
+  variants: {
+    state: {
+      default: "",
+      selected: "bg-purple-500 text-white",
+      disabled: "cursor-not-allowed bg-slate-500 text-slate-400",
+      unavailable: "cursor-not-allowed bg-red-200 text-red-300",
+    },
+    hoverable: {
+      true: "hover:bg-purple-50",
+    },
+  },
+  defaultVariants: {
+    state: "default",
   },
 });
 
@@ -30,18 +46,17 @@ export function CalendarTable(props: CalendarGridProps) {
       <CalendarGridBody>
         {(date) => (
           <CalendarCell
-            // TODO: How to do this with tailwind variants?
             className={({ isDisabled, isUnavailable, isSelected }) =>
-              tw(
-                "mx-auto my-0.5 flex aspect-square w-8 items-center justify-center rounded-full text-sm font-semibold text-gray-600 sm:w-11",
-                !isUnavailable &&
-                  !isDisabled &&
-                  !isSelected &&
-                  "hover:bg-purple-50",
-                isDisabled && "cursor-not-allowed bg-slate-500 text-slate-400",
-                isUnavailable && "cursor-not-allowed bg-red-200 text-red-300",
-                isSelected && "bg-purple-500 text-white",
-              )
+              calendarCell({
+                state: isSelected
+                  ? "selected"
+                  : isDisabled
+                    ? "disabled"
+                    : isUnavailable
+                      ? "unavailable"
+                      : "default",
+                hoverable: !isUnavailable && !isDisabled && !isSelected,
+              })
             }
             date={date}
           />
