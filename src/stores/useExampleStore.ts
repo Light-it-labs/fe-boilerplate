ec36446fa9336d7444afaff35ee62c9aed063ccc
walt-it <72817685+walt-it@users.noreply.github.com>
2024-10-17 16:03:56 -0300

    Use stores standard on auth store (#41)
    * feat: adjust auth store to standard
    
    * bugfix: fix import
    
    * feat: restore namespace
    
    * feat: revert unwanted change

--- src/stores/useExampleStore.ts
+++ src/stores/useExampleStore.ts
@@ -1,11 +1,7 @@
 /**
- * This file adheres to our standard for Zustand stores without using `persist`.
+ * This file adheres to our standard for Zustand stores.
  * The state and actions are externalized, promoting modularity and separation of concerns,
  * as outlined in Zustand's best practices: https://zustand.docs.pmnd.rs/guides/practice-with-no-store-actions.
- *
- * We use a namespace in the barrel file (e.g., export * as authStore from "./useAuthStore")
- * to gather everything in one place, achieving both objectives: keeping concerns separate while avoiding
- * multiple setters with similar names, as often seen in component states or sections.
  *
  * This structure simplifies state management and ensures consistency across the project.
  */
@@ -18,17 +14,21 @@ export type ExampleStoreState = {
   thirdValue: number | null;
 };
 
-export const useStore = create<ExampleStoreState>()(() => ({
+const useStore = create<ExampleStoreState>(() => ({
   firstValue: null,
   secondValue: null,
   thirdValue: null,
 }));
 
-export const setFirstValue = () =>
-  useStore.setState((state) => ({ firstValue: state.firstValue }));
+export const getFirstValue = () => useStore((state) => state.firstValue);
+export const getSecondValue = () => useStore((state) => state.secondValue);
+export const getThirdValue = () => useStore((state) => state.thirdValue);
 
-export const setSecondValue = () =>
-  useStore.setState((state) => ({ secondValue: state.secondValue }));
+export const setFirstValue = (firstValue: ExampleStoreState["firstValue"]) =>
+  useStore.setState(() => ({ firstValue }));
 
-export const setThirdValue = () =>
-  useStore.setState((state) => ({ thirdValue: state.thirdValue }));
+export const setSecondValue = (secondValue: ExampleStoreState["secondValue"]) =>
+  useStore.setState(() => ({ secondValue }));
+
+export const setThirdValue = (thirdValue: ExampleStoreState["thirdValue"]) =>
+  useStore.setState(() => ({ thirdValue }));
